b6200c440d43871bf422ee6feae148ccf002a5aa
nopounch <nopounch@gmail.com>
2019-05-29 20:32:26 -0400

    Created messenger: system which updates simplecies based on faces and offset, sends message to task messenger

--- golog.py
+++ golog.py
@@ -9,6 +9,9 @@ from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
 
+#globals
+updatekeytoint = {0:'pos'}
+
 class golog(ShowBase):
     def __init__(self,*args, **kwargs):
         ShowBase.__init__(self)
@@ -31,69 +34,103 @@ class golog(ShowBase):
         f = self.createMorphism((b,a),label = 'f')
         g = self.createMorphism((c,b),label = 'g')
 
-        #self.taskMgr.add(lambda t: self.obMoverTask(b,t), "mover task")
+        self.taskMgr.add(self.simpMoverTask, "mover task", extraArgs = [b,b.data['node'].getPos()], appendTask = True)
+        self.taskMgr.add(self.simpMoverTask, "mover task", extraArgs = [a,a.data['node'].getPos()], appendTask = True)
+
         #self.taskMgr.add(lambda t: self.morMoverTask(f,t),"moverTask")
-        self.taskMgr.add(self.cameraTask,"camera Task")
+        #self.taskMgr.add(self.cameraTask,"camera Task")
         #self.sphere.ls()
 
     #create a new simplex an bind graphics
     #pass graphics attributes and simplex kwargs
     def createObject(self, *args, **kwargs):
-        numsimps = len(self.sSet.rawSimps)
-
         #create a simplex in the simplicial set
         simplex = self.sSet.add(0,*args, **kwargs)
 
         #create an instance of simplex graphics in golog, send to simplex.data['gr']
         simplexGr = self.render.attachNewNode(simplex.label+" Node")
-        simplex.data['gr'] = simplexGr
+        simplex.data['node'] = simplexGr
         self.sphere.instanceTo(simplexGr)
 
-        defaults = {'setPos':(2*numsimps,0,0)}
+        #create a messenger name for simplex (for now just rawSimps index)
+        simplex.data['_messengerName'] = 'simp' + str(self.sSet.rawSimps.index(simplex))
+
+        defaults = {'setPos':(0,0,0)}
         for key in defaults.keys():
             if key in kwargs.keys(): getattr(simplexGr,key)(kwargs[key])
             else: getattr(simplexGr,key)(defaults[key])
         #simplexGr.ls()
         return simplex
 
+
+    def updateSimp(self,simp,kwargs = dict()):
+        #since the task manager doesn't let you pass kwargs, you have to pass a number and index by that number
+
+        ####TODO: Make this method accept either kwargs or multiple args with some index
+
+        # kwargs = dict()
+        # for n in range(len(ntypes)): kwargs = kwargs + {inttokey[i]:}
+        #
+        #
+        # kwargs = [{inttokey[i],} for i in ntypes]
+
+        if 'pos' in kwargs:
+            #update morphism to be average position of it's faces nodes + some offset passed through kwargs['pos']
+            pos = kwargs['pos']
+            n = len(simp.faces)
+            for f in simp.faces: pos = pos + f.data['node'].getPos()/n #transform offset pos to offset from average of faces
+            simp.data['node'].setPos(pos)
+            self.messenger.send( simp.data['_messengerName']+' moved', [{'pos':Point3(0,0,0)}]) #sending (0,0,0) just updates the child nodes by default
+            if simp.level > 0: print("yoyoyoyoyoy")
+
+
     def createMorphism(self, faces, *args, **kwargs):
         dom = faces[1]
         codom = faces[0]
         simplex = self.sSet.add(faces,*args,**kwargs)
-        simplexGr = Rope()
-        middlenode = simplexGr.attachNewNode("middlenode")
-        middlenode.setPos((dom.data['gr'].getPos()+codom.data['gr'].getPos())/2)
+        simplex.data['_messengerName'] = 'simp' + str(self.sSet.rawSimps.index(simplex))
+        # offset for middlenode
+
+
+
+        rope = Rope()
+        middlenode = self.gologNode.attachNewNode("middlenode")
+        simplex.data['node'] = middlenode
+        middlenode.setPos((dom.data['node'].getPos()+codom.data['node'].getPos())/2)
+
+
+        #create a middlenode listener for face movements
+        ######
+        for f in simplex.faces: self.accept(f.data['_messengerName'] + ' moved',self.updateSimp,[simplex])
+        #####
 
         #set start and endpoint to be the domain and codomain graphics
+        rope.setup(3,[(dom.data['node'],(0,0,0)),
+                    (simplex.data['node'],(0,0,0)),
+                    (codom.data['node'],(0,0,0))])
+        rope.reparentTo(self.gologNode)
+        simplex.data['graphics'] = rope
 
-        simplexGr.setup(3,[(dom.data['gr'],(0,0,0)),
-                    (middlenode,(0,0,0)),
-                    (codom.data['gr'],(0,0,0))])
-        simplexGr.reparentTo(self.render)
-        simplex.data['gr'] = simplexGr
-        print(simplexGr.getPoints(3))
+
         return simplex
 
+    # def simpMove(self, simp, pos):
+    #     simp.data['graphics'].setPos(pos)
+    #     #get average Positions
+    #     self.messenger.send(simp.data['_messengerName'] + ' moved',[simp.data['_messengerName'] + ' moved'])
 
-    def obMoverTask(self,ob,task):
+    def simpMoverTask(self,simp,orig,task):
         t = task.time
-        curve = lambda t: Point3(5+3*sin(10*t),0,3*sin(10*t))
-        ob.data['gr'].setPos(curve(t))
-        return task.cont
-
-    def morMoverTask(self,mor,task):
-        t = task.time
-        curve = lambda t: Point3(5+3*sin(10*t),0,3*sin(10*t))
-        #get middlenode
-        mor.data['gr'].getChild(0).setPos(curve(t))
+        curve = lambda t: Point3(5*sin(t),5*sin(t),5*sin(t))
+        self.updateSimp(simp, {'pos' : orig + curve(t)})
         return task.cont
 
 
-    def cameraTask(self, task):
-        t = task.time
-        self.camera.setPos(50*cos(t),50*sin(t),0)
-        self.camera.lookAt(self.sSet.rawSimps[0].data['gr'])
-        return task.cont
+    # def cameraTask(self, task):
+    #     t = task.time
+    #     self.camera.setPos(50*cos(t),50*sin(t),50*sin(t))
+    #     self.camera.lookAt(self.sSet.rawSimps[0].data['graphics'])
+    #     return task.cont
 
 app = golog()
 app.run()
