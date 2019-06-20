f6ee384c46283108a583c3d8ed49e35adb57af10
Noah Grant Chrein <nchrein@umd.edu>
2019-06-20 16:59:07 -0400

    began creating a golog export library, creates a directed simplicial set that stores string call data to reconstruct golog from scratch after unpickling

--- golog_export.py
+++ golog_export.py
@@ -1,18 +1,29 @@
 import pickle
 import os, sys
 from hcat import simpSet
-
+sSet = None
 def gexport(golog):
     sSet = simpSet(label = golog.label+"_save")
     render = golog.render
 
+    def transform_to_python_and_get_children(node):
+        # if node in golog.NPtoSimplex.keys():
+        #     data = golog.NPtoSimplex()
+        sSet.add(0,label = str(type(node))) #add a 0-simplex with label the node-type
+        if node.getNumChildren() > 0:
+            for child in node.getChildren():
+                transform_to_python_and_get_children(child)
+
+    transform_to_python_and_get_children(render)
     rundiags(sSet)
 
-def gimport(gologProjection,base):
-    pass
+def gimport(sSet, base = None):
+    if base == None:
+        sSet = sSet
+        return
 
 
 
 ################# DIAGNOSTICS FOR EXPORT MODULE ###################
 def rundiags(sSet):
-    print(sSet.label)
+    print([s.label for s in sSet.rawSimps])
